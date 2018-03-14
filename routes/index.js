1206029ba01e0e3eb365d5310ff6e0d4beaf998a
Bryan Jenkins <bryjenk00@gmail.com>
2018-03-14 16:49:47 -0500

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Classic Masonry ltd blaaaa' });
+  res.render('index', { title: 'Classic Masonry' });
 });
 
 module.exports = router;
